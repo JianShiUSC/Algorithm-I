6e8bca1c92c8509110620597f02be290e4fa0245
Jian Shi <jians@usc.edu>
2016-03-18 20:50:16 -0700

    QuickSort

--- Quick.java
+++ Quick.java
@@ -34,3 +34,23 @@ public class Quick
         sort(a, j+1, hi);
     }
 }
+
+
+//3-way quicksort
+private static void sort(Comparable[] a, int lo, int hi)
+{
+    if(hi <= lo)    return;
+    int lt = lo, gt = hi;
+    Comparable v = a[lo];
+    int i = lo;
+    while(i <= gt)
+    {
+        int cmp = a[i].compareTo(v);
+        if     (cmp < 0)    exch(a, lt++, i++);
+        else if(cmp > 0)    exch(a, i, gt--);
+        else    i++;
+    }
+
+    sort(a, lo, lt - 1);
+    sort(a, gt + 1, hi);
+}
